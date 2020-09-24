986f683dc7f2d63998517c1728a1d9d7a683d850
Philip <37758269+katzcentrality@users.noreply.github.com>
2020-09-24 10:58:59 -0400

    push to heroku

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Dashboard from './components/Auth/Dashboard';
 import CourtFinder from './apis/CourtFinder';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Container } from "@material-ui/core";
+
 
 
 toast.configure();
@@ -20,7 +22,9 @@ function App() {
 
 const [isAuth, setIsAuth] = useState(false);
 
-  const setAuth = boolean => {
+const [isLoading, setIsLoading] = useState(true);
+
+const setAuth = boolean => {
     setIsAuth(boolean);
   };
   
@@ -30,8 +34,9 @@ const [isAuth, setIsAuth] = useState(false);
 async function isAuthenticated() {
 try {
   const response = await CourtFinder.get('/is-verified', {headers: {token: localStorage.token}})
-  // const setter = response.data;
+  console.log(response)
   response.data === true ? setIsAuth(true) : setIsAuth(false)
+  setIsLoading(false);
 } 
 catch (err) {
 console.log(err);
@@ -39,25 +44,56 @@ console.log(err);
 }
 
 useEffect(() => {
-isAuthenticated()
+isAuthenticated();
 }, []);
 
+
+// const reviewCheck = () => {
+//   if (isLoading) return "Please wait."
+//   if (!isAuth) return <Redirect to="/login" />
+//   if (isAuth) return <Route exact path="/courts/:id/addReview" component={AddReview} />
+// }
 
   
   return (
     
     <CourtsContextProvider>
+      <Container>
     <Router>
     <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/courts/:id/update" component={UpdatePage} />
     <Route exact path="/courts/:id" component={CourtdetailPage} /> 
-    <Route exact path="/courts/:id/addReview" component={AddReview} />
+    
+
+    <Route
+  exact
+  path="/courts/:id/addReview"
+  render={(props) =>
+    !isAuth ? (
+      <Login {...props} setAuth={setAuth} />
+    ) : (
+      <>
+      <Switch>
+        <Redirect exact from="/" to="/courts/:id/addReview" />
+        <Route exact path="/courts/:id/addReview">
+          <AddReview />
+        </Route>
+        </Switch>
+      </>
+    )
+  }
+/>
+
+    {/* test route */}
+    {/* <Route exact path="/courts/:id/addReview" component={AddReview}/> */}
+
     <Route exact path="/login" render={props => !isAuth ? (<Login {...props} setAuth={setAuth} />) : ( <Redirect to="/dashboard" />)} />
     <Route exact path="/register" render={props => !isAuth ? (<Register {...props} setAuth={setAuth} /> ) : ( <Redirect to="/dashboard" />)} />
     <Route path="/dashboard" render={props => isAuth ? ( <Dashboard {...props} setAuth={setAuth} />) : ( <Redirect to="/login" />)} />
     </Switch>
     </Router>
+    </Container>
     </CourtsContextProvider>
  
 
